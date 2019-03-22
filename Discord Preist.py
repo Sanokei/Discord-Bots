366093c2bf4797d09c87033d4a2132a61b2b16ee
Sano <36809574+Sanokei@users.noreply.github.com>
2019-03-22 18:29:27 -0400

    Update Discord Preist.py

--- Discord Preist.py
+++ Discord Preist.py
@@ -25,37 +25,38 @@ async def on_member_join(member):
 
 @bot.event
 async def on_message(message):
-    lines = ["It's okay my son","Go on my son","I forgive your sins","Your sins have been lifted","Forgive yourself before I can start forgiving you"]
-    blacklist = ["blacklisted peoples id"]
-    lowermessage = message.content.lower()
-    BannedWords = ["fuck"]
-    server_channel = discord.Object("channel id")
-    #open file
-    file= open("sins.txt","a+")
-    #get id of author
-    client = await bot.get_user_info(message.author.id)
-    for i in range(0,len(BannedWords)):
-        if BannedWords[i] in lowermessage:
-            await bot.send_message(client, "This is a christian server")
+    if(str(message.channel.type) == "private"):
+        lines = ["It's okay my son","Go on my son","I forgive your sins","Your sins have been lifted","Forgive yourself before I can start forgiving you"]
+        blacklist = ["blacklisted peoples id"]
+        lowermessage = message.content.lower()
+        BannedWords = ["fuck"]
+        server_channel = discord.Object("channel id")
+        #open file
+        file= open("sins.txt","a+")
+        #get id of author
+        client = await bot.get_user_info(message.author.id)
+        for i in range(0,len(BannedWords)):
+            if BannedWords[i] in lowermessage:
+                await bot.send_message(client, "This is a christian server")
+                return
+        if client in blacklist:
+            await bot.send_message(client, "You have been blacklisted for a non-determained amount of time")
             return
-    if client in blacklist:
-        await bot.send_message(client, "You have been blacklisted for a non-determained amount of time")
-        return
-    #check if bot speaks
-    if message.author == bot.user:
-        return
-    #send to dm after message was sent
-    rand = random.randrange(len(lines))
-    await bot.send_message(client, lines[rand])
-    
-    if(server_channel == message.channel):
-        return
-    #saving what people said
-    print("{}: {}\n".format(client,message.content))
-    file.write("{}: {}\n".format(client,message.content))      
-    #send it to a server
-    confession_message = "Anonymous: " + message.content
-    await bot.send_message(server_channel,confession_message)
+        #check if bot speaks
+        if message.author == bot.user:
+            return
+        #send to dm after message was sent
+        rand = random.randrange(len(lines))
+        await bot.send_message(client, lines[rand])
+
+        if(server_channel == message.channel):
+            return
+        #saving what people said
+        print("{}: {}\n".format(client,message.content))
+        file.write("{}: {}\n".format(client,message.content))      
+        #send it to a server
+        confession_message = "Anonymous: " + message.content
+        await bot.send_message(server_channel,confession_message)
             
-bot.run("Api Key");
+bot.run("Token");
     
